5268849733d3644011e6343d2d7e1352b3f38cd7
Jaakko Kuusela <jaakko.kuusela@outlook.com>
2024-08-05 15:30:40 +0300

    add main.py and move program launch there from cli.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from maria import Maria
+from cli import CLI
+
+UI_names = ['CLI', 'QT']
+#choose the UI based on the command line arguments
+UI_name = 'CLI' #default UI
+if len(sys.argv) > 1: #if command line arguments have been passed:
+    if sys.argv[1] in UI_names:
+        UI_name = sys.argv[1]
+    else:
+        sys.exit('Failed to provide acceptable command line argument for UI')
+
+
+match UI_name:
+    case 'CLI':
+        UI = CLI()
+                
+    case 'QT':
+        pass
+                
+maria = Maria(UI=UI)
+maria.session()
+
