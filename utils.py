fc019a0d1c0417878c497cbd7d300d1a76e69adc
Jaakko Kuusela <jaakko.kuusela@outlook.com>
2024-08-07 15:12:13 +0300

    add utils.py and few file handling functions into it

---
+++ utils.py
@@ -0,0 +1,20 @@
+#utils.py
+
+import os
+import shutil
+
+def backup_file(file_path):
+    if os.path.exists(file_path):
+        backup_path = f'{file_path}.{datetime.now().strftime('%Y%m%d%H%M%S')}.bak'
+        shutil.copy(file_path, backup_path)
+        
+
+def save_to_file(file_path, data):
+    with open(file_path, 'w') as file:
+        file.write(data)
+
+
+#excluded should be a list or a tuple        
+def to_dict(obj, excluded):
+    return {key: value for key, value in obj.__dict__.items() if key not in excluded}
+
